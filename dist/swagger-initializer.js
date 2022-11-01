1c2c5576d5b447110be78d807bb4e8cb2c2cb6ec
Mike Meier <mike@gotom.io>
2022-11-01 18:35:01 +0400

    styling

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "swagger.yaml",
+    url: "gotom-api-doc.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
@@ -13,7 +13,8 @@ window.onload = function() {
     plugins: [
       SwaggerUIBundle.plugins.DownloadUrl
     ],
-    layout: "StandaloneLayout"
+    layout: "StandaloneLayout",
+    supportedSubmitMethods: [],
   });
 
   //</editor-fold>
